bb5205d4f9badae3b90fc2c6dd16c2ff96a56268
Jon Narine <jnwnarine@gmail.com>
2021-12-24 13:27:30 -0500

    import MainMenu to dragonrendpage

--- src/pages/DragonRend.js
+++ src/pages/DragonRend.js
@@ -4,14 +4,12 @@ import BugsPage from './BugsPage';
 import AboutPage from './AboutPage';
 //COMPONENTS
 import DragonRendBanner from '../components/DragonRendBanner';
-
+import MainMenu from '../components/MainMenu';
 function DragonRend() {
   return (
   <div>
     <DragonRendBanner/>
-    
-    
-    
+    <MainMenu/>
   </div>
   );
 }
