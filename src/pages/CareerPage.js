6a740e7f93e87eb5a13f0ffc34de5014a7500b07
Jon Narine <jnwnarine@gmail.com>
2021-12-27 11:50:21 -0500

    mainmenu links and basic styling done

---
+++ src/pages/CareerPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CareerPage = () => {
+    return (
+        <div>
+            random career stat crap
+
+        </div>
+    )
+};
+
+export default CareerPage;
