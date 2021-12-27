7335d871688cd91dfe4fc4e6c68d21d28a136b5a
Jon Narine <jnwnarine@gmail.com>
2021-12-26 23:20:16 -0500

    added email to newgameform

---
+++ src/components/forms/NewGameForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NewGameForm = () => {
+    return (
+        <form>
+            <label for="heroName">What is your name, Hero?</label>
+            <input required type="text" name="heroName"/>
+
+            <label for="email">An email account, please!</label>
+            <input required type="text" name="email"/>
+            {/* when email is input, render "Would you like to recieve game and account updates?"
+            also optional recovery email */}
+        </form>
+    )
+};
+
+export default NewGameForm;
+
